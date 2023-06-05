3b4b6e9436b44960ac9e862498c48ffb693d2174
TimotheeRapin <61775714+TimotheeRapin@users.noreply.github.com>
2023-06-05 14:15:00 +0200

    Version finale

--- Code/View/home.php
+++ Code/View/home.php
@@ -6,7 +6,7 @@
  * @author                  Created by Timothée RAPIN
  * Creation date            05.05.2021
  * update                   05.05.2021
- * @version                 1.0
+ * @version                 1.1
  * @note                    Creation of this file
  */
 // Start output buffering
@@ -19,17 +19,14 @@ $title = 'Application web pour la gestion des repas en lien avec les commissions
 <!-- Page content -->
 <main id="page" class="container">
     <h1>Accueil</h1>
+
     <p>
-        Bienvenue sur l'application web pour la gestion des repas en lien avec les commissions et le budget.
+        Bienvenue!
     </p>
     <p>
-        Cette plateforme permet de g^érer son budget en fonction des repas que l'on souhaite faire. <br>
-        Elle gènère une liste de commissions en 2 types de listes. <br>
-        Soit un liste pour l'enseigne la moin cher, soit avec les produits les moins cher, peut importe l'enseigne. <br>
-    </p>
-    <p>
-        Il est possible de créer des menu avec les plats que l'on souhaite. <br>
-        Pour chaque plat, il est possible de choisir le nombre de personne pour lequel le plat est prévu. <br>
+        Cette plateforme vous permet de générer une liste de commissions la plus économique en fonction des prix dans chaque enseigne.<br>
+        Il est possible de créer des menus avec les plats que l'on souhaite.<br>
+        Pour chaque plat, il est possible de choisir le nombre de personnes pour lequel il est prévu.<br>
     </p>
 </main>
 
